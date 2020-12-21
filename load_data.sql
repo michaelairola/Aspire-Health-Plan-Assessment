8493e30b59dbcab4dd5849e8d1b34ae61f3b037c
michaelairola <mikeairola@mac.com>
2020-12-21 15:32:42 -0800

    Woops! format needs %Y, I had %y

--- load_data.sql
+++ load_data.sql
@@ -1,14 +1,14 @@
 LOAD DATA LOCAL INFILE "./data/Eligibility table.txt" INTO TABLE eligibility
 	IGNORE 1 LINES
 (member_id, name, @start_date, @end_date)
-SET start_date = STR_TO_DATE(@start_date,'%m/%d/%y'), end_date = STR_TO_DATE(@end_date,'%m/%d/%y');
+SET start_date = STR_TO_DATE(@start_date,'%m/%d/%Y'), end_date = STR_TO_DATE(@end_date,'%m/%d/%Y');
 
 LOAD DATA LOCAL INFILE "./data/HEDIS table.txt" INTO TABLE hedis
 	IGNORE 1 LINES
 	(member_id, hedis_measure, @date_of_service, result)
-SET date_of_service = STR_TO_DATE(@date_of_service, '%m/%d/%y');
+SET date_of_service = STR_TO_DATE(@date_of_service, '%m/%d/%Y');
 
 LOAD DATA LOCAL INFILE "./data/Claims table.txt" INTO TABLE claims 
 	IGNORE 1 LINES
 	(member_id, hedis_measure, procedure_code, @date_of_service)
-SET date_of_service = STR_TO_DATE(@date_of_service, '%m/%d/%y');
+SET date_of_service = STR_TO_DATE(@date_of_service, '%m/%d/%Y');
