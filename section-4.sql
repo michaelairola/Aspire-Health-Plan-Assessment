9480fcafc073b816d0a0126583134ed852c09224
michaelairola <mikeairola@mac.com>
2020-12-21 15:34:10 -0800

    make sure start_date is before end_date, and also add a day to start_date the proper way (need to use DATE_ADD fn)

--- section-4.sql
+++ section-4.sql
@@ -1,10 +1,10 @@
 WITH RECURSIVE cte AS (
     SELECT name, start_date, end_date
-    FROM eligibility
+    FROM eligibility WHERE start_date < IFNULL(end_date, "9999-12-31")
     UNION ALL
     SELECT e.name, cte.start_date, e.end_date
     FROM cte
-    JOIN eligibility e ON cte.name = e.name and cte.end_date < e.start_date + 1
+    JOIN eligibility e ON cte.name = e.name and IFNULL(cte.end_date, "9999-12-31") <= DATE_ADD(e.start_date, INTERVAL 1 DAY)
 ), cte2 as (
     SELECT name, start_date, end_date, row_number() over (partition by name, end_date ORDER BY start_date) AS rn
     FROM cte
